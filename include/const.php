70d670b79f0b68d0c164c0689ca979874ed54583
McAngelo <mcangelo200@gmail.com>
2017-12-15 19:51:49 +0000

    --added the live database connection credentials

--- include/const.php
+++ include/const.php
@@ -1,8 +1,14 @@
 <?php
-		//Database Constants
+		//Local Database Constants
 		define("DB_HOST", "localhost");
 		define("DB_USER", "webdeveloper");
 		define("DB_PASS", "mich120");
 		define("DB_NAME", "db_firmpoll");
+
+		//Live Database Constants
+		/*define("DB_HOST", "heroku_6000bc17fe7afc1");
+		define("DB_USER", "b9020e9240ef84");
+		define("DB_PASS", "2db7b9aa");
+		define("DB_NAME", "db_firmpoll");*/
 
 ?>
