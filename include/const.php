1066e686416bba1420aa772e0a09ce95839a8c0d
McAngelo <mcangelo200@gmail.com>
2017-12-18 18:11:56 +0000

    --added travis continious integration build manager

--- include/const.php
+++ include/const.php
@@ -1,14 +1,14 @@
 <?php
 		//Local Database Constants
-		define("DB_HOST", "localhost");
+		/*define("DB_HOST", "localhost");
 		define("DB_USER", "webdeveloper");
 		define("DB_PASS", "mich120");
-		define("DB_NAME", "db_firmpoll");
+		define("DB_NAME", "db_firmpoll");*/
 
 		//Live Database Constants
-		/*define("DB_HOST", "heroku_6000bc17fe7afc1");
+		define("DB_HOST", "us-cdbr-iron-east-05.cleardb.net");
 		define("DB_USER", "b9020e9240ef84");
 		define("DB_PASS", "2db7b9aa");
-		define("DB_NAME", "db_firmpoll");*/
+		define("DB_NAME", "heroku_6000bc17fe7afc1");
 
 ?>
